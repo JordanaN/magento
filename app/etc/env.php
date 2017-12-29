bb90147d1f010eaeb459de5f0cf7bdf013ffc092
Jordana Nogueira <jordanasnogueira@gmail.com>
2017-12-29 00:42:37 -0200

    env

---
+++ app/etc/env.php
@@ -0,0 +1,59 @@
+<?php
+return array (
+  'backend' =>
+  array (
+    'frontName' => 'admin',
+  ),
+  'crypt' =>
+  array (
+    'key' => 'fcf23f625e048c86fdf5bf71607fbf9b',
+  ),
+  'db' =>
+  array (
+    'table_prefix' => '',
+    'connection' =>
+    array (
+      'default' =>
+      array (
+        'host' => 'bfjrxdpxrza9qllq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        'dbname' => 'rz09b4disuo1svtk',
+        'username' => 'cb9mqkfov4w683e8',
+        'password' => 'tl3qo0f6z5ytcdi4',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' =>
+  array (
+    'default_setup' =>
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'session' =>
+  array (
+    'save' => 'files',
+  ),
+  'cache_types' =>
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'customer_notification' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'full_page' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+  ),
+  'install' =>
+  array (
+    'date' => 'Fri, 29 Dec 2017 01:08:40 +0000',
+  ),
+);
